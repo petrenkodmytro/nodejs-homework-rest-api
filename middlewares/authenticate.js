a899a9f5ca11a42981b2cda8f59d9bee47d34da2
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-07-27 15:53:17 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -9,7 +9,7 @@ const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
 
-  if (bearer !== "Bearer" && token === undefined) {
+  if (bearer !== "Bearer" || !token) {
     next(HttpError(401, "Not authorized. Not token"));
   }
   try {
